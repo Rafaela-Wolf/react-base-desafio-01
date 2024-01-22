845f8bb394f72b112cf03cc6630b7a60d810bc2d
Rafaela Wolf <rafaelawolf52@gmail.com>
2024-01-22 09:21:10 -0300

    first commit

---
+++ src/components/paragraph/paragraph.js
@@ -0,0 +1,14 @@
+import React from "react";
+import './paragraph.css';
+
+class Paragraph extends React.Component {
+    render() {
+        return (
+            <div className="paragraph">
+                <p>Lorem ipsum dolor sit amet consectetur, adipisicing elit. Unde veritatis voluptate nisi ea at libero tempora est quisquam. Quod, eius! Illum placeat odit perferendis, praesentium dolorum laboriosam cupiditate minima reiciendis.</p>
+            </div>
+        )
+    }
+}
+
+export default Paragraph
